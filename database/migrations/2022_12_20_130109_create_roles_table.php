ddb743d981392a2d9d0a196e7a5639056d0ca9d9
unknown11 <kesvesna@rambler.ru>
2022-12-20 17:18:11 +0300

    Создана таблица ролей и их заполнение по дефолту.

---
+++ database/migrations/2022_12_20_130109_create_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->uuid();
+            $table->string('name', 100)->unique();
+            $table->string('comment')->nullable()->default(null);
+            $table->integer('sort_order')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+};
