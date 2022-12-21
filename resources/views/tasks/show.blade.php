b113b65a7541e0f306d2d1faa94fcc870e1f0551
granal1 <granal1@mail.ru>
2022-12-21 15:28:16 +0200

    bootstrap через cdn

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,12 +2,12 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta lang="ru">
     <title>Задача</title>
+    <meta lang="ru">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
