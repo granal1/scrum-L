41c97c90a2368ca3e0729b0925258b079a274289
unknown11 <kesvesna@rambler.ru>
2022-12-21 17:03:34 +0300

    Исправил название линков на Главная

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Создание задачи</title>
+    <title>Редактирование задачи</title>
     <meta lang="ru">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,7 +21,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <a class="nav-link active" aria-current="page" href="/">Главная</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Link</a>
