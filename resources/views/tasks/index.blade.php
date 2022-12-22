2d440a32d4e13006c70ee532a85b2452b2ff5bee
unknown11 <kesvesna@rambler.ru>
2022-12-22 17:31:40 +0300

    Подключил новую страницу карточки сотрудника

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -23,6 +23,9 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 @auth
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('users.create')}}">Профайл</a>
+                    </li>
                 </ul>
                 @endauth
                 <ul class="navbar-nav mb-2 mb-lg-0">
