6924a7531f1047500ccf4b2e73e46ae53b9562c1
unknown11 <kesvesna@rambler.ru>
2022-12-22 15:39:47 +0300

    Add fortify in project, registration off

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,10 +15,11 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
+            @auth
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
+            @endauth
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 @auth
