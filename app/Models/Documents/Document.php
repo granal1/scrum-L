dabe424d361936f8bf29b7994d6c2f4545259e34
kesvesna <kesvesna@rambler.ru>
2022-12-25 17:00:17 +0300

    Добавил фильтр документов по названию и пути

--- app/Models/Documents/Document.php
+++ app/Models/Documents/Document.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Documents;
 
+use App\Models\Traits\Filterable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Document extends Model
 {
-    use HasFactory, SoftDeletes, HasUuids;
+    use HasFactory, SoftDeletes, HasUuids, Filterable;
 
     protected $table = "files";
 
@@ -18,5 +19,15 @@ class Document extends Model
         'path',
         'comment'
     ];
+
+    protected function removeQueryParam(string ...$keys)
+    {
+        foreach($keys as $key)
+        {
+            unset($this->queryParams[$key]);
+        }
+
+        return $this;
+    }
 
 }
