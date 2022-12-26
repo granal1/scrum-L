3b386041bd80a44bffb1b076734f1f711ed3564f
unknown11 <kesvesna@rambler.ru>
2022-12-26 13:03:31 +0300

    Добавил возможность выбирать роль сотрудника, если у него роль делопроизводитель, то он может создавать новые документы, кнопку Добавить документ пока видят все

--- app/Http/Requests/Users/UpdateUserFormRequest.php
+++ app/Http/Requests/Users/UpdateUserFormRequest.php
@@ -24,7 +24,6 @@ class UpdateUserFormRequest extends FormRequest
     public function rules()
     {
         return [
-            'login' => ['required', 'string', 'min:1', 'max:100'],
             'name' => ['required', 'string', 'min:2', 'max:100'],
             'email' => ['required', 'string', 'min:3', 'max:100'],
             'phone' => ['nullable', 'string', 'min:3', 'max:15'],
@@ -32,7 +31,8 @@ class UpdateUserFormRequest extends FormRequest
             'password' => ['nullable', 'string', 'min:2', 'max:50'],
             'comment' => ['nullable', 'string', 'min:1', 'max:3000'],
             'superior_uuid' => ['nullable', 'string', 'min:36', 'max:36'],
-            'subordinate_uuid' => ['nullable', 'string', 'min:36', 'max:36']
+            'subordinate_uuid' => ['nullable', 'string', 'min:36', 'max:36'],
+            'role_uuid' => ['required', 'string', 'min:36', 'max:36'],
         ];
     }
 
