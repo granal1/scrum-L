92c3aa2e0f865c2a89d94b13d1fe9a652686f9c6
Vlad <vladison@list.ru>
2022-12-27 23:26:09 +0300

    page_design_changed

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,53 +1,52 @@
- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-     <div class="container-fluid">
-         <img class="navbar-brand" alt="Navbar picture" src="{{asset('assets/icons/navbar_icon.svg')}}" width="45" height="45">
-         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-             <span class="navbar-toggler-icon"></span>
-         </button>
+<nav class="navbar navbar-expand-md navbar-light bg-light shadow-sm">
+    <div class="container">
+        <img class="navbar-brand" alt="Navbar picture" src="{{asset('assets/icons/navbar_icon.svg')}}" width="45" height="45">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
 
-         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-             @auth
-                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                     <li class="nav-item">
-                         <a class="nav-link {{ request()->routeIs('tasks.*')?'active':'' }}" href="{{ route('tasks.index') }}">Задачи</a>
-                     </li>
-                     @can('viewAny', \App\Models\Documents\Document::class)
-                     <li class="nav-item">
-                         <a class="nav-link {{ request()->routeIs('documents.*')?'active':'' }}" href="{{ route('documents.index') }}">Документы</a>
-                     </li>
-                     @endcan
-                     @can('viewAny', \App\Models\Users\User::class)
-                     <li class="nav-item">
-                         <a class="nav-link {{ request()->routeIs('users.*')?'active':'' }}" href="{{ route('users.index') }}">Сотрудники</a>
-                     </li>
-                     @endcan
-                     <li class="nav-item">
-                         <a class="nav-link {{ request()->routeIs('users.show')?'active':'' }}" href="{{ route('users.show', Auth::id()) }}">Профиль</a>
-                     </li>
-                     @can('viewAny', \App\Models\Admin\Admin::class)
-                     <li class="nav-item">
-                         <a class="nav-link {{ request()->routeIs('admin.*')?'active':'' }}" href="{{ route('admin.index') }}">Админка</a>
-                     </li>
-                     @endcan
-                 </ul>
-             @endauth
-             <ul class="navbar-nav mb-2 mb-lg-0">
-                 @auth
-                     <li class="nav-item">
-                         <form action="/logout" method="post">
-                             @csrf
-                             <button class="btn btn-info btn-sm" type="submit">Выход</button>
-                         </form>
-                     </li>
-                 @endauth
-                 @guest
-                     <li class="nav-item">
-                         <a class="nav-link {{ request()->routeIs('login')?'active':'' }}" href="/login">Вход</a>
-                     </li>
-                 @endguest
-             </ul>
-         </div>
-     </div>
- </nav>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            @auth
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('tasks.*')?'active':'' }}" href="{{ route('tasks.index') }}">Задачи</a>
+                </li>
+                @can('viewAny', \App\Models\Documents\Document::class)
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('documents.*')?'active':'' }}" href="{{ route('documents.index') }}">Документы</a>
+                </li>
+                @endcan
+                @can('viewAny', \App\Models\Users\User::class)
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('users.*')?'active':'' }}" href="{{ route('users.index') }}">Сотрудники</a>
+                </li>
+                @endcan
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('users.show')?'active':'' }}" href="{{ route('users.show', Auth::id()) }}">Профиль</a>
+                </li>
+                @can('viewAny', \App\Models\Admin\Admin::class)
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('admin.*')?'active':'' }}" href="{{ route('admin.index') }}">Админка</a>
+                </li>
+                @endcan
+            </ul>
+            @endauth
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                @auth
+                <li class="nav-item">
+                    <form action="/logout" method="post">
+                        @csrf
+                        <button class="btn btn-secondary btn-sm" type="submit">Выход</button>
+                    </form>
+                </li>
+                @endauth
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('login')?'active':'' }}" href="/login">Вход</a>
+                </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
