25d0ffe69e03fc1a3e254ed3c1455e33c402a2f3
granal1 <granal1@mail.ru>
2023-01-11 16:17:42 +0200

    Изменены миграции tasks, task_files

--- database/migrations/2022_12_21_072853_create_task_files_table.php
+++ database/migrations/2022_12_21_072853_create_task_files_table.php
@@ -16,9 +16,10 @@ return new class extends Migration
         Schema::create('task_files', function (Blueprint $table) {
             $table->uuid('id')->primary();
 
-            $table->foreignUuid('file_uuid');
             $table->foreignUuid('task_uuid');
-
+            $table->foreignUuid('file_uuid');
+//            $table->foreignUuid('output_file_uuid');
+// TODO Раскомментировать (3 места), когда будет сделана таблица output_files
 
             $table->string('comment')->nullable()->default(null);
             $table->integer('sort_order')->nullable()->default(1);
@@ -28,8 +29,9 @@ return new class extends Migration
 
         Schema::table('task_files', function($table)
         {
-            $table->foreign('file_uuid')->references('id')->on('files')->onupdate('cascade')->ondelete('no action');
             $table->foreign('task_uuid')->references('id')->on('tasks')->onupdate('cascade')->ondelete('no action');
+            $table->foreign('file_uuid')->references('id')->on('files')->onupdate('cascade')->ondelete('no action');
+//            $table->foreign('output_file_uuid')->references('id')->on('output_files')->onupdate('cascade')->ondelete('no action');
 
         });
     }
@@ -44,6 +46,7 @@ return new class extends Migration
         Schema::table('task_files', function (Blueprint $table) {
             $table->dropForeign(['file_uuid']);
             $table->dropForeign(['task_uuid']);
+//            $table->dropForeign(['output_file_uuid']);
         });
 
         Schema::dropIfExists('task_files');
