792626696655f1d0851325d823a86355794c9580
unknown11 <kesvesna@rambler.ru>
2023-01-13 09:03:33 +0300

    Подправил триггеры создания и редактирования задач, для записи в логи

--- database/migrations/2023_01_12_202623_create_log_task_table.php
+++ database/migrations/2023_01_12_202623_create_log_task_table.php
@@ -14,17 +14,18 @@ return new class extends Migration
     public function up()
     {
         Schema::create('log_task', function (Blueprint $table) {
-            $table->id();
-            $table->string('parent_uuid', 36);
-            $table->string('priority_uuid', 36);
+            $table->uuid('id')->primary();
+            $table->string('task_uuid', 36);
+            $table->string('parent_uuid', 36)->nullable()->default(null);
+            $table->string('priority_uuid', 36)->nullable()->default(null);
             $table->string('author_uuid', 36);
             $table->string('responsible_uuid', 36);
             $table->text('description');
             $table->timestamp('deadline_at');
-            $table->integer('done_progress');
-            $table->text('report');
+            $table->integer('done_progress')->nullable()->default(null);
+            $table->text('report')->nullable()->default(null);
             $table->integer('sort_order');
-            $table->string('comment');
+            $table->string('comment')->nullable()->default(null);
             $table->timestamps();
             $table->softDeletes();
         });
