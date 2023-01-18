6474ece65f18e96ad67acea863918ace4e64435d
unknown11 <kesvesna@rambler.ru>
2023-01-18 17:27:31 +0300

    Добавил возможность после выполнения задачи исполнителю прикрепить зарегистрированный на его имя как исполнителя исходящий документ

--- public/assets/tasks/progress_bar_value_display.js
+++ public/assets/tasks/progress_bar_value_display.js
@@ -14,6 +14,17 @@ window.onload = function() {
 }
 
 setProgressBarValue = function(){
-    document.getElementById('progress_bar_value').value = document.getElementById('done_progress').value;
+
+    let progress_value = document.getElementById('done_progress').value;
+    document.getElementById('progress_bar_value').value = progress_value;
+
+    let outgoing_files_block = document.getElementById('outgoing_files_block');
+
+    if(progress_value > 99)
+    {
+        outgoing_files_block.classList.remove("d-none");
+    } else {
+        outgoing_files_block.classList.add("d-none");
+    }
 }
 
