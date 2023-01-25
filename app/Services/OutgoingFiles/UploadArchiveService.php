6adadad9e9bdd9382826ac004652730d6558783d
granal1 <granal1@mail.ru>
2023-01-25 23:05:40 +0200

    Идентичное имя документа и приложения с уч. Час/П.

--- app/Services/OutgoingFiles/UploadArchiveService.php
+++ app/Services/OutgoingFiles/UploadArchiveService.php
@@ -8,13 +8,14 @@ use Symfony\Component\HttpFoundation\File\Exception\UploadException;
 
 class UploadArchiveService
 {
-    public function uploadMedia(UploadedFile $uploadedFile): string
+    public function uploadMedia(UploadedFile $uploadedFile, $now): string
     {
-        $path = $uploadedFile->storeAs('files/output_files/archives/' . date('Y/m/d'), date('Ymd-His') . '.zip', 'public');
+        $path = $uploadedFile->storeAs('files/output_files/archives/' . 
+            date_format($now,"Y/m/d"),
+            date_format($now,"Ymd-His") . '.zip', 'public');
 
         if ($path === false) {
             Log::error('Архив не удалось загрузить на диск');
-
         }
 
         return $path;
