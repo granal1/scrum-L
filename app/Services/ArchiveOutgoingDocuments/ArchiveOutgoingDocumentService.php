125ff76b81847b6ca166aa0598f93c5aab2260a4
kesvesna <kesvesna@rambler.ru>
2023-02-17 18:37:40 +0300

    Исправил ошибку, падение при отсутствии архивных таблиц.

--- app/Services/ArchiveOutgoingDocuments/ArchiveOutgoingDocumentService.php
+++ app/Services/ArchiveOutgoingDocuments/ArchiveOutgoingDocumentService.php
@@ -3,6 +3,7 @@
 namespace App\Services\ArchiveOutgoingDocuments;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class ArchiveOutgoingDocumentService
 {
@@ -13,26 +14,40 @@ class ArchiveOutgoingDocumentService
         $date = strtotime(date('Y') . ' -1 year');
         $result[] = date('Y', $date);
 
-        foreach (DB::select('SHOW TABLES LIKE "archive_outgoing_files_%"') as $item) {
-            foreach ($item as $key => $value) {
-                $result[] = substr($value, -4);
+        if(Schema::hasTable('archive_outgoing_files_%'))
+        {
+            foreach (DB::select('SHOW TABLES LIKE "archive_outgoing_files_%"') as $item) {
+                foreach ($item as $key => $value) {
+                    $result[] = substr($value, -4);
+                }
+            }
+
+            if(!empty($result)){
+                arsort($result);
+                return $result;
             }
         }
-        arsort($result);
-        return $result;
+
+        return [];
     }
 
     public function getLastArchiveYear(): string
     {
-        foreach (DB::select('SHOW TABLES LIKE "archive_outgoing_files_%"') as $item) {
-            foreach ($item as $key => $value) {
-                $result[] = substr($value, -4);
+        if(Schema::hasTable('archive_outgoing_files_%')) {
+            foreach (DB::select('SHOW TABLES LIKE "archive_outgoing_files_%"') as $item) {
+                foreach ($item as $key => $value) {
+                    $result[] = substr($value, -4);
+                }
+            }
+            if (!empty($result)) {
+                arsort($result);
+
+                $years = array_reverse($result);
+                return array_pop($years);
             }
         }
-        arsort($result);
 
-        $years = array_reverse($result);
-        return array_pop($years);
+        return '';
     }
 
 
