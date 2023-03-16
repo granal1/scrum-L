a29ccf1a543798c06993d1442780f4bdda3926e9
granal1 <granal1@mail.ru>
2023-03-17 00:29:59 +0200

    Действие при ошибке 419 (просрочен CSRF)

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -3,3 +3,21 @@
 @section('title', __('Page Expired'))
 @section('code', '419')
 @section('message', __('Page Expired'))
+
+{{
+    Log::error('Ошибка 419',
+    [
+        'user' => Auth::user()->name ?? 'Page Expired',
+        'url' => $_SERVER['HTTP_HOST'],
+    ])
+}}
+
+{{
+ Request::session()->invalidate(),
+ Request::session()->regenerateToken(),
+ header('Refresh:1; http://'.$_SERVER['HTTP_HOST'].'/'),
+ exit
+}}
+
+{{-- как вариант:  header('Refresh:3; http://'.$_SERVER['HTTP_HOST'].'/') --}}
+{{-- как вариант:  header('location: http://'.$_SERVER['HTTP_HOST'].'/') --}}
