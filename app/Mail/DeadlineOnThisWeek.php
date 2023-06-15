bddb4b2410258435da424c28b140676dc7b90ed1
granal1 <granal1@mail.ru>
2023-06-15 13:28:41 +0200

    Испр. Адрес 'from' для рассылки уведомлений

--- app/Mail/DeadlineOnThisWeek.php
+++ app/Mail/DeadlineOnThisWeek.php
@@ -37,9 +37,10 @@ class DeadlineOnThisWeek extends Mailable
     {
 
         return new Envelope(
-            from: new Address('kesvesna@projects-collection.ru', 'Делопроизводство'),
-            subject: 'Запланированные задачи',
-        );
+            from: new Address('granal1@mail.ru', 'Электронное делопроизводство'),
+            //from: new Address(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME')),
+            subject: 'Добавлена новая задача',
+        );  //TODO Реализовать вариант с env
     }
 
     /**
