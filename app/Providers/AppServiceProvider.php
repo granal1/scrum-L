3faa482cdef8baa531e25dc49dfebba8e92ca4b6
granal1 <granal1@mail.ru>
2023-06-29 23:11:17 +0200

    Доб. Установка локализации Carbon при старте

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Carbon::setLocale('ru_Ru');
         Paginator::useBootstrapFive();
     }
 }
